f2ff656dc9305856862cacbe4941a6b86bd0754e
Shawn <shawncolby@billericak12.com>
2019-10-09 12:04:18 -0400

    fghdsgf

---
+++ src/com/company/Fraction.java
@@ -0,0 +1,84 @@
+package com.company;
+
+public class Fraction {
+    private int numerator;		//attributes
+    private int denominator;
+
+    // constructor
+    public Fraction(int n, int d)	// constructor
+    {
+        numerator = n;
+        denominator = d;
+    }
+
+    public Fraction (){
+        numerator = 0;
+        denominator = 0;
+    }
+
+    public int getNum()
+    {
+        return numerator;
+    }
+    public int getDenom()
+    {
+        return denominator;
+    }
+    public Fraction add(Fraction other)
+    {
+        int n = this.numerator*other.denominator + this.denominator*other.numerator;
+        int d = this.denominator * other.getDenom();
+
+        Fraction result = new Fraction(n,d);
+        return result;
+    }
+
+    public Fraction subtract(Fraction other){
+        int n = this.numerator*other.denominator - this.denominator*other.numerator;
+        int d = this.denominator * other.getDenom();
+
+        Fraction result = new Fraction(n,d);
+        return result;
+    }
+
+    public Fraction multiply(Fraction other){
+        int n = this.numerator*other.numerator;
+        int d = this.denominator*other.getDenom();
+
+        Fraction result = new Fraction(n,d);
+        return result;
+    }
+
+    public String toString() {
+        String s = numerator + "/" + denominator;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(3,4);	// create an instance of fraction
+        Fraction f2 = new Fraction(4,5);
+        Fraction f6 = new Fraction(6,9);
+        Fraction f7 = new Fraction(9,6);
+
+        Fraction f3 = f1.add(f2);			// add 2 fractions
+        System.out.println("3/4 + 4/5 = " + f3);	// print the answer
+
+		Fraction f4 = f1.subtract(f2);
+		System.out.println(f1 + " - " + f2 + " = " + f4);
+
+		Fraction f5 = f1.multiply(f2);
+		System.out.println(f1 + " x " + f2 + " = " + f5);
+
+		Fraction f8 = f6.add(f7);
+        System.out.println(f6 + " + " + f7 + " = " + f8);
+
+
+
+
+
+
+
+
+    }
+
+}
